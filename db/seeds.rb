73250f4f65cca0bf6cfb73dc4d4a43fb3268ce84
George Redinger <george@georgeredinger.com>
2010-09-05 15:40:12 -0700

    added big movie to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ vid = Video.create( :url => "http://georgeredinger.s3.amazonaws.com/nano.mp4", :
 vid = Video.create( :url => "nano-private.mp4", :title =>"cloud nano with timeout", :owner =>   "Timely Clouded Programmer", :description =>   "private (query string authentication) S3 delivery", :delivery  => :s3_querystring)
 vid = Video.create( :url => "nano-private.mp4", :title =>"secret nano with timeout", :owner =>   "Secret Clouded Programmer", :description =>   "private (query string authentication) send_file delivery", :delivery  => :s3_send_file)
 vid = Video.create( :url => "http://georgeredinger.s3.amazonaws.com/nano.mp4", :title =>"video from remote url", :owner =>   "Secret Clouded Programmer", :description =>   "send a public url as a private file", :delivery  => :url_send_file)
+vid = Video.create( :url => "http://mirror.bigbuckbunny.de/peach/bigbuckbunny_movies/big_buck_bunny_720p_h264.mov", :title =>"big video from remote url", :owner =>   "Big Buck Bunny Rabbit", :description =>   "this could cause havoc with the tmp storage", :delivery  => :url_send_file)
 
 
 
